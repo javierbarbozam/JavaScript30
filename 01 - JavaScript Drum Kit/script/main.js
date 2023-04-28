be634df68b6a766763563667fc7226a88f3c1750
javierbarbozam <barboza.javier497@gmail.com>
2023-04-27 22:53:50 -0600

    finish exercise

--- 01 - JavaScript Drum Kit/script/main.js
+++ 01 - JavaScript Drum Kit/script/main.js
@@ -1,7 +1,24 @@
 window.addEventListener('keydown', event => {
-	const audio = document.querySelector(`audio[data-key="${event.code}"]`)
-	if (audio) {
-		audio.currentTime = 0;
-		audio.play()
-	}
+  const audio = document.querySelector(`audio[data-key="${event.code}"]`)
+  // Plays if there is a code linked to an audio element
+  if (audio) {
+    audio.currentTime = 0; // makes the audio restart every time the key is pressed
+    audio.play()
+    keyStyle(event.code)
+  }
 })
+
+const keyStyle = (value) => {
+  const keys = document.querySelectorAll('.key-item')
+  // Style the pressed key
+  keys.forEach(key => {
+    if (value === key.attributes[0].value){
+      key.classList.add('key-item--play');
+      key.addEventListener('transitionend', removeTransition)
+    }
+  })
+}
+
+function removeTransition () {
+  this.classList.remove('key-item--play')
+}
